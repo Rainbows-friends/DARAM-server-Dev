289590c32ec1f021895f2a7a748278c342e752a4
snowykte0426 <snowykte0426@naver.com>
2024-12-19 12:11:38 +0900

    chore: 요청 전체 허용 설정
    개발 서버에서 모든 엔드포인트에 대하여 인가를 내주도록 접근권한을 수정하였습니다

--- src/main/kotlin/rainbowfriends/daramserverdev/global/security/config/DomainAuthorizationConfig.kt
+++ src/main/kotlin/rainbowfriends/daramserverdev/global/security/config/DomainAuthorizationConfig.kt
@@ -1,6 +1,5 @@
 package rainbowfriends.daramserverdev.global.security.config
 
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
 import org.springframework.stereotype.Component
@@ -9,37 +8,6 @@ import org.springframework.stereotype.Component
 class DomainAuthorizationConfig {
     fun configure(authorizeRequests: AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry) {
         authorizeRequests
-            .requestMatchers(
-                HttpMethod.GET,
-                "/notice/all",
-                "/notice/{id}",
-                "/actuator/health",
-                "/checkin/checkin",
-                "/checkin/uncheckin",
-                "/camera/authorization"
-            ).permitAll()
-            .requestMatchers(
-                HttpMethod.POST,
-                "/notice/**"
-            ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
-            .requestMatchers(
-                HttpMethod.PUT,
-                "/notice/**"
-            ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
-            .requestMatchers(
-                HttpMethod.PATCH,
-                "/notice/**"
-            ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
-            .requestMatchers(
-                HttpMethod.DELETE,
-                "/notice/**"
-            ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
-            .requestMatchers(
-                "/actuator/**"
-            ).hasRole("DEVELOPER")
-            .requestMatchers(
-                "/checkin"
-            ).hasAnyRole("ADMIN", "TEACHER", "DEVELOPER")
             .anyRequest().permitAll()
     }
 }
