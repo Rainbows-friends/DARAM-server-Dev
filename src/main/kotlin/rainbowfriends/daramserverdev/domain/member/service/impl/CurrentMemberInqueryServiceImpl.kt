0575c0c6745f218f1cdddba9564c78413adeec4c
snowykte0426 <snowykte0426@naver.com>
2024-12-26 15:35:49 +0900

    refactor: ``throw`` 관련 연산 추가
    헤더가 없을 경우 예외를 발행하도록 하였습니다

--- src/main/kotlin/rainbowfriends/daramserverdev/domain/member/service/impl/CurrentMemberInqueryServiceImpl.kt
+++ src/main/kotlin/rainbowfriends/daramserverdev/domain/member/service/impl/CurrentMemberInqueryServiceImpl.kt
@@ -2,6 +2,7 @@ package rainbowfriends.daramserverdev.domain.member.service.impl
 
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Service
+import rainbowfriends.daramserverdev.domain.auth.exception.TokenNotFoundException
 import rainbowfriends.daramserverdev.domain.member.exception.MemberNotFoundException
 import rainbowfriends.daramserverdev.domain.member.service.CurrentMemberInqueryService
 import rainbowfriends.daramserverdev.global.member.component.FindMember
@@ -15,9 +16,11 @@ class CurrentMemberInqueryServiceImpl(
 ) : CurrentMemberInqueryService {
     override fun getCurrentMember(request: HttpServletRequest): MemberDTO {
         return findMember.findMemberByEmail(
-            jwtTokenParserService.extractUserId(
-                request.getHeader("Authorization").substring(7)
-            )
+            if (request.getHeader("Authorization") == null) throw TokenNotFoundException("Token not found")
+            else
+                jwtTokenParserService.extractUserId(
+                    request.getHeader("Authorization").substring(7)
+                )
         )
             ?.toDto() ?: throw MemberNotFoundException("Member not found")
     }
