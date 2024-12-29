a94698b51c28c2aba5a38858cec5a1df406f6945
snowykte0426 <snowykte0426@naver.com>
2024-12-29 22:42:05 +0900

    test: 스케쥴링 작동 확인을 위한 스케쥴링 시각 변경
    지각 횟수를 증분시키는 로직의 테스트를 위하여 스케쥴링 설정된 시각을 변경하였습니다

--- src/main/kotlin/rainbowfriends/daramserverdev/global/checkin/component/CheckInDataScheduler.kt
+++ src/main/kotlin/rainbowfriends/daramserverdev/global/checkin/component/CheckInDataScheduler.kt
@@ -29,7 +29,7 @@ class CheckInDataScheduler(
         checkInRepository.deleteAll(checkInRepository.findByCheckinInfoDate(cutoffDate))
     }
 
-    @Scheduled(cron = "0 35 22 * * *")
+    @Scheduled(cron = "0 48 22 * * *")
     fun scheduledCheckInDataSync() {
         val today: LocalDate = LocalDate.now()
         val tomorrow: LocalDate = today.plusDays(1)
