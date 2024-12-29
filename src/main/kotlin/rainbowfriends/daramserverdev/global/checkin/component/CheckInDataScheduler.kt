abcc320c053d2bd335edd33fe7ada55d8551f54f
snowykte0426 <snowykte0426@naver.com>
2024-12-29 22:17:19 +0900

    test: 지각 횟수 증분 로직 로깅 강화
    지각 횟수를 증분시키는 로직의 테스트를 위하여 스케쥴링 설정된 시각을 변경하였습니다

--- src/main/kotlin/rainbowfriends/daramserverdev/global/checkin/component/CheckInDataScheduler.kt
+++ src/main/kotlin/rainbowfriends/daramserverdev/global/checkin/component/CheckInDataScheduler.kt
@@ -29,7 +29,7 @@ class CheckInDataScheduler(
         checkInRepository.deleteAll(checkInRepository.findByCheckinInfoDate(cutoffDate))
     }
 
-    @Scheduled(cron = "0 16 22 * * *")
+    @Scheduled(cron = "0 24 22 * * *")
     fun scheduledCheckInDataSync() {
         val today: LocalDate = LocalDate.now()
         val tomorrow: LocalDate = today.plusDays(1)
