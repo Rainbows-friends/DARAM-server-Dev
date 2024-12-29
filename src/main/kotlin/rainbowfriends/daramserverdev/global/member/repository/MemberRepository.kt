fffa4719d67c73adf44cc64e0e2e9566a848668b
snowykte0426 <snowykte0426@naver.com>
2024-12-29 21:28:22 +0900

    feat: 쿼리문 직접 기입 및 캐싱 Key 변경
    캐싱 키 중복으로 인한 오류를 피하기 위하여 캐싱 Key를 변경하였습니다

--- src/main/kotlin/rainbowfriends/daramserverdev/global/member/repository/MemberRepository.kt
+++ src/main/kotlin/rainbowfriends/daramserverdev/global/member/repository/MemberRepository.kt
@@ -2,12 +2,14 @@ package rainbowfriends.daramserverdev.global.member.repository
 
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import rainbowfriends.daramserverdev.global.member.entity.Member
 
 @Repository
 interface MemberRepository : JpaRepository<Member, Long?> {
-    @Cacheable(value = ["MemberByStudentId"], key = "#grade + #classNum + #number")
+    @Cacheable(value = ["MemberByStudentId"], key = "T(String).format('%d%d%02d', #grade, #classNum, #number)")
+    @Query("SELECT m FROM Member m WHERE m.grade = :grade AND m.classNum = :classNum AND m.number = :number")
     fun findByGradeAndClassNumAndNumber(grade: Int, classNum: Int, number: Int): Member?
     @Cacheable(value = ["MemberByEmail"], key = "#email")
     fun findByEmail(email: String?): Member?
